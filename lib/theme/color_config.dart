87fa5c8bcf99ae3ac69f63d2ef76120f503ff9f6
Desrielkiki <desriel7c6@gmail.com>
2025-02-11 04:42:45 +0700

    :wrench: modify color config

--- lib/theme/color_config.dart
+++ lib/theme/color_config.dart
@@ -1,7 +1,7 @@
 class ColorConfig {
-  static const primaryColor = 0xFF3F51B5;
-  static const onPrimaryColor = 0xFF3F51B5;
-  static const secondaryColor = 0xFF3F51B5;
-  static const onSecondaryColor = 0xFF3F51B5;
+  static const primaryColor = 0x242b4a;
+  static const onPrimaryColor = 0x323c66;
+  static const secondaryColor = 0x374066;
+  static const onSecondaryColor = 0x455080;
   static const red = 0xffFF0000;
 }
