21154ac10229d90bce8fa1aa37d4dc1a6c09870a
Desrielkiki <desriel7c6@gmail.com>
2025-02-11 02:22:41 +0700

    :hammer:create font style

---
+++ lib/theme/textstyle_config.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:lynkco/theme/color_config.dart';
+
+class TextstyleConfig {
+  static TextStyle largeOnPrimary({
+    Color color = const Color(ColorConfig.onPrimaryColor),
+    double size = 14.0,
+    FontWeight weight = FontWeight.normal,
+    FontStyle style = FontStyle.normal,
+  }) {
+    return TextStyle(
+      color: color,
+      fontSize: size,
+      fontWeight: weight,
+      fontStyle: style,
+    );
+  }
+
+  static TextStyle largeOnSecondary({
+    Color color = const Color(ColorConfig.onSecondaryColor),
+    double size = 14.0,
+    FontWeight weight = FontWeight.normal,
+    FontStyle style = FontStyle.normal,
+  }) {
+    return TextStyle(
+      color: color,
+      fontSize: size,
+      fontWeight: weight,
+      fontStyle: style,
+    );
+  }
+}
