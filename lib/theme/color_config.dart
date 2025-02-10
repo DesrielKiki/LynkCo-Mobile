9db7a2fedb521a1fa6f353b77f6200910eddd3b4
Desrielkiki <desriel7c6@gmail.com>
2025-02-11 01:55:25 +0700

    :hammer: create color config

---
+++ lib/theme/color_config.dart
@@ -0,0 +1,6 @@
+class ColorConfig {
+  static const primaryColor = 0xFF3F51B5;
+  static const onPrimaryColor = 0xFF3F51B5;
+  static const secondaryColor = 0xFF3F51B5;
+  static const onSecondaryColor = 0xFF3F51B5;
+}
