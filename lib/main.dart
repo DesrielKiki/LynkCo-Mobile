a8b0ab7d1ad49165dec253afd3c127e8a161046e
Desrielkiki <desriel7c6@gmail.com>
2025-02-19 15:06:32 +0700

    :wrench:modify register screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:lynkco/screen/auth/register_screen.dart';
 import 'package:lynkco/theme/color_config.dart';
 import 'package:lynkco/widget/auth_button.dart';
 import 'package:lynkco/widget/auth_support_text.dart';
 import 'package:lynkco/widget/auth_textfield.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  final TextEditingController _controller = TextEditingController();
-
-  MyApp({super.key});
+  const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -20,27 +19,6 @@ class MyApp extends StatelessWidget {
           colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
           useMaterial3: true,
         ),
-        home: Scaffold(
-          backgroundColor: ColorConfig.secondaryColor,
-          body: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                AuthTextfield(
-                    labelText: "labelText",
-                    icon: Icons.ac_unit,
-                    obscureText: true,
-                    controller: _controller,
-                    keyboardType: TextInputType.multiline),
-                const SizedBox(
-                  height: 32,
-                ),
-                const AuthButton(buttonText: "buttonText"),
-                const AuthSupportText(
-                    firstText: "firstText?", secondText: "secondText")
-              ],
-            ),
-          ),
-        ));
+        home: const RegisterScreen());
   }
 }
